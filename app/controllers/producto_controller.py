75248d26cf50218b5b8e2e04eda859a156620b80
Howard Anti <155449787+HowardAnti@users.noreply.github.com>
2024-06-10 17:46:15 +0000

    Avance 10/06/2024

--- app/controllers/producto_controller.py
+++ app/controllers/producto_controller.py
@@ -4,11 +4,11 @@ from app.models.producto_model import Producto
 from app.utils.decorators import jwt_required, roles_required
 from app.views.producto_view import render_product_detail, render_product_list
 
-# Crear un blueprint para el controlador de animales
+
 producto_bp = Blueprint("producto", __name__)
 
 
-# Ruta para obtener la lista de animales
+
 @producto_bp.route("/products", methods=["GET"])
 @jwt_required
 @roles_required(["admin", "user"])
@@ -17,7 +17,7 @@ def get_productos():
     return jsonify(render_product_list(productos))
 
 
-# Ruta para obtener un animal específico por su ID
+
 @producto_bp.route("/products/<int:id>", methods=["GET"])
 @jwt_required
 @roles_required(["admin", "user"])
@@ -28,7 +28,7 @@ def get_producto(id):
     return jsonify({"error": "Producto no encontrado"}),404
 
 
-# Ruta para crear un nuevo animal
+
 @producto_bp.route("/products", methods=["POST"])
 @jwt_required
 @roles_required(["admin"])
@@ -38,18 +38,18 @@ def create_productol():
     description=data.get("description")
     price=data.get("price")
     stock=data.get("stock")
-    # Validación simple de datos de entrada
+
     if not name or not description or price is None or stock is None:
         return jsonify({"error": "Faltan datos requeridos"}), 400
 
-    # Crear un nuevo animal y guardarlo en la base de datos
+
     producto=Producto(name,description, price,stock)
     producto.save()
 
     return jsonify(render_product_detail(producto)), 201
 
 
-# Ruta para actualizar un animal existente
+
 @producto_bp.route("/products/<int:id>", methods=["PUT"])
 @jwt_required
 @roles_required(["admin"])
@@ -70,7 +70,7 @@ def update_producto(id):
     return jsonify(render_product_detail(producto))
 
 
-# Ruta para eliminar un animal existente
+
 @producto_bp.route("/products/<int:id>", methods=["DELETE"])
 @jwt_required
 @roles_required(["admin"])
